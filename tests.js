6297d7a3f1c88437f2faaf7f7f048eb292caf7ec
Kevin Orta <orta.kevin@gmail.com>
2021-08-24 14:05:02 -0500

    feat: added true/false hello world to test

--- tests.js
+++ tests.js
@@ -33,4 +33,13 @@ describe("sayHello", function () {
     it('should return the string "Hello, Pat!" when executed', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it('should  return the string "Hello, World!" when executed', function () {
+        expect(sayHello("World")).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when true', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World! when false', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 });
